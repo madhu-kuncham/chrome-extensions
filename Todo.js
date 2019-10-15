27a9c2a9ca3c1993d0df7fa4d0a9a1bd5322c0bb
pranavi179 <pranavivelisoju@gmail.com>
2019-10-15 12:31:00 +0530

    increased the extension width

--- Todo.js
+++ Todo.js
@@ -1,3 +1,13 @@
+
+'use strict';
+
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.storage.sync.set({color: '#3aa757'}, function() {
+    console.log("The color is green.");
+  });
+});
+
+
 const addForm = document.querySelector('.add');
 const list = document.querySelector('.Todos');
 
