19fdc5971982fc075cfa5befaf74f28cc91a11cb
pranavi179 <pranavivelisoju@gmail.com>
2019-10-15 13:19:23 +0530

    changes made

--- Todo.js
+++ Todo.js
@@ -4,8 +4,7 @@
 chrome.runtime.onInstalled.addListener(function() {
   chrome.storage.sync.set({color: '#3aa757'}, function() {
     console.log("The color is green.");
-  });
-});
+ 
 
 
 const addForm = document.querySelector('.add');
@@ -23,8 +22,10 @@ const generateTemplate = Todo => {
 list.innerHTML += html;
 
 };
+// document.getElementById('form1').addEventListener('submit', function(evt){
+// document.getElementById('addForm').addEventListener('submit',function(e){
+    addForm.addEventListener('submit',e =>{
 
-addForm.addEventListener('submit',e =>{
 
     e.preventDefault();
     const Todo = addForm.addEventListener.value.trim();
@@ -35,3 +36,5 @@ addForm.addEventListener('submit',e =>{
     }
 });
 
+});
+});
