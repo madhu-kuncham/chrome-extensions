7a8fa28528017d3d8d8dde0311b8c650dda4ddfc
KUNCHAM MADHU KUMAR <53073559+madhu-kuncham@users.noreply.github.com>
2019-10-15 11:23:32 +0530

    Add files via upload

---
+++ Todo.js
@@ -0,0 +1,27 @@
+const addForm = document.querySelector('.add');
+const list = document.querySelector('.Todos');
+
+const generateTemplate = Todo => {
+
+    const html = `
+    <li class ="list-group-item d-flex justify-content-between align-items-center">
+       <span>Todo</span>
+       <i class="fas fa-trash-alt delete">
+    </i>
+`;
+
+list.innerHTML += html;
+
+};
+
+addForm.addEventListener('submit',e =>{
+
+    e.preventDefault();
+    const Todo = addForm.addEventListener.value.trim();
+
+    if(Todo.length){
+        generateTemplate(Todo);
+        addForm.reset();
+    }
+});
+
